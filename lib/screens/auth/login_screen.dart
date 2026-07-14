17b27d9b6f51023472c1ab453c8fc0cb041274bc
Chisomaga Anyasodor <chisomanyasodor@gmail.com>
2026-07-14 16:07:25 +0100

    Crate login screen UI

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,17 +1,62 @@
 import 'package:flutter/material.dart';
 
+import '../../constants/app_colors.dart';
+import '../../constants/app_text_styles.dart';
+
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Login")),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
 
-      body: const Center(
-        child: Text(
-          "Welcome to FloodGuard",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+            const Icon(Icons.water_drop, size: 80, color: AppColors.primary),
+
+            const SizedBox(height: 20),
+
+            const Text("Welcome Back", style: AppTextStyles.heading),
+
+            const SizedBox(height: 10),
+
+            const Text(
+              "Login to continue monitoring flood risks",
+              style: AppTextStyles.body,
+              textAlign: TextAlign.center,
+            ),
+
+            const SizedBox(height: 40),
+
+            TextField(
+              decoration: const InputDecoration(
+                labelText: "Email Address",
+                prefixIcon: Icon(Icons.email),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            TextField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: "Password",
+                prefixIcon: Icon(Icons.lock),
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            ElevatedButton(
+              onPressed: () {},
+
+              child: const Text("LOGIN", style: AppTextStyles.button),
+            ),
+          ],
         ),
       ),
     );
